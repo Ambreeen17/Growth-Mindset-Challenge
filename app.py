b04951c6b0c95ab31ad3c230d0d4ca5aa0800e07
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2025-04-16 14:30:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from io import BytesIO
 # Set up our app
 st.set_page_config(page_title="My App", layout="wide")
 st.title("Data Sweeper")
-st.write("Welcome to my app! I’m Tahirah Roohi, building a data-cleaning app using Streamlit.")
+st.write("Welcome to my app! I’m Ambreen Rais, building a data-cleaning app using Streamlit.")
 
 uploaded_file = st.file_uploader("Upload Your Files (CSV or Excel)", type=["csv", "xlsx"], accept_multiple_files=True)
 
